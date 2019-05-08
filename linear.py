29f7d00e48bfa4dcfdb89e0d6154b364453759c7
Jiefor <49774360+Jiefor@users.noreply.github.com>
2019-05-08 12:57:11 +0800

    0508

--- linear.py
+++ linear.py
@@ -16,47 +16,80 @@ desired_caps['appPackage']='com.meilin.wulianbaogj'
 desired_caps['appActivity']='.activity.SplashActivity'
 desired_caps['unicodeKeyboard']=True
 desired_caps['resetKeyboard']=True
-
+desired_caps['automationName']='Uiautomator2'
 #远程调起app
 dr=webdriver.Remote("http://127.0.0.1:4723/wd/hub",desired_caps)
 time.sleep(1)
 #弹窗
+#dr.switch_to.alert.accept()#该方法未实现，咱不可用
 dr.find_element_by_id('android:id/button1').click()
 time.sleep(2)
 #进入登录页面，进行登录操作
 dr.find_element_by_id('com.meilin.wulianbaogj:id/et_login_username').send_keys('13500000001')
-time.sleep(1)
+time.sleep(2)
 dr.find_element_by_id('com.meilin.wulianbaogj:id/et_login_password').send_keys('a12345678')
-time.sleep(1)
+time.sleep(2)
 dr.find_element_by_id('com.meilin.wulianbaogj:id/login_mian_btn').click()
-time.sleep(3)
+time.sleep(4)
 #上滑
 dr.swipe(20,1700,20,200,2000)
 #在线报事
 dr.find_element_by_id('com.meilin.wulianbaogj:id/rl_baoshi').click()
+time.sleep(2)
 dr.find_element_by_id('com.meilin.wulianbaogj:id/content').send_keys('test')
+time.sleep(2)
 #最小化键盘
 dr.hide_keyboard()
-#滑动UP
-dr.swipe(20,1700,20,200,2000)#第一次滑动
-dr.swipe(20,1700,20,1000,2000)#第二次滑动
+time.sleep(2)
+# -*-#滑动UP
+# dr.swipe(20,1700,20,200,2000)#第一次滑动
+# time.sleep(1)
+# dr.swipe(20,1700,20,1000,2000)#第二次滑动
+# time.sleep(2)
 # #添加照片
-# dr.find_element_by_xpath('')
-# dr.find_element_by_name('拍摄').click()
+# TouchAction(dr).press(x=200,y=450).release().perform()
+# time.sleep(2)
+# dr.find_element_by_id('android:id/button1').click()
+# time.sleep(2)
+# #dr.switch_to_alert().accept()#弹框允许，暂不可以用方法未实现
+# dr.find_element_by_id('com.android.camera:id/v9_shutter_button_internal').click()
+# time.sleep(5)
 # dr.find_element_by_id('com.android.camera:id/inten_done_apply').click()
 # time.sleep(10)
 # #添加视频
-# dr.find_elements_by_id('com.meilin.wulianbaogj:id/recyclerViewPics')[4].click()
-# dr.find_element_by_id('com.android.camera:id/v9_shutter_button_internal').click()
-# time.sleep(4)
-# dr.find_element_by_id('com.android.camera:id/v9_shutter_button_internal').click()
+# #滑动UP
+# dr.swipe(20,1700,20,200,2000)#第一次滑动
+# time.sleep(1)
+# dr.swipe(20,1700,20,1000,2000)#第二次滑动
+# time.sleep(2)
+# TouchAction(dr).press(x=220,y=840).release().perform()
+# time.sleep(2)
+# # dr.find_element_by_id('android:id/button1').click()
+# # time.sleep(2)
+# #开始
+# TouchAction(dr).press(x=550,y=1700).release().perform()
+# time.sleep(5)
+# #结束录制
+# TouchAction(dr).press(x=550,y=1700).release().perform()
+# time.sleep(3)
+# #上传
 # dr.find_element_by_id('com.android.camera:id/inten_done_apply').click()
 # time.sleep(30)
 # #添加录音
-# el=dr.find_element_by_id('com.meilin.wulianbaogj:id/talkBtn')
-# TouchAction.long_press(el,100,1100,4000).perform().release()
+
+#滑动UP
+dr.swipe(20,1700,20,200,2000)#第一次滑动
+time.sleep(1)
+dr.swipe(20,1700,20,1000,2000)#第二次滑动
+time.sleep(2)
+
+time.sleep(3)
+TouchAction(dr).long_press(500,1200,4000).perform().release()
+time.sleep(2)
 #添加备注
 dr.find_element_by_id('com.meilin.wulianbaogj:id/remarkET').send_keys('testdata')
+time.sleep(2)
 #提交
-dr.find_element_by_xpath("//*[@class='android.widget.Button']").click()
+dr.find_element_by_class_name('android.widget.Button').click()
 time.sleep(5)
+
